b7627eee0ce77695932c06e803fe2515ac0b0134
root <grisha@somoto.net>
2017-11-27 01:12:09 +0200

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeRecordComponent } from './home-record/home-record.component';
 import { ListRecordComponent } from './list-record/list-record.component';
 
+import  { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -19,8 +21,11 @@ import { ListRecordComponent } from './list-record/list-record.component';
     ListRecordComponent
   ],
   imports: [
+    MaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
